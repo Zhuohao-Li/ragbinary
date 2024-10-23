567905828a04ca186d14835f8e1b3ffb1b0a0130
zhuohaol <zhuohaol@amazon.com>
2024-10-23 15:21:57 -0700

    fix bugs in showing the similarity

--- query_data.py
+++ query_data.py
@@ -4,6 +4,10 @@ from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
+import os
+
+os.environ['OPENAI_API_KEY'] = "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"
+
 
 CHROMA_PATH = "./contextual_rag/chroma"
 # chroma helps to build a vectorDB
@@ -18,33 +22,42 @@ Answer the question based only on the following context:
 Answer the question based on the above context: {question}
 """
 
-
-def main():
+def parse():
     # Create CLI.
     parser = argparse.ArgumentParser()
     parser.add_argument("query_text", type=str, help="The query text.")
     args = parser.parse_args()
     query_text = args.query_text
+    return query_text
 
+def retrieve_db(query_text):
     # Prepare the DB.
     embedding_function = OpenAIEmbeddings()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    results = db.similarity_search_with_relevance_scores(query_text, k=20)
+    return results
+    
+def print_scores(results):
     similarity_scores = [score for _, score in results]
     print(similarity_scores)
     if len(results) == 0 or results[0][1] < 0.7:
         print(f"Unable to find matching results.")
         return
 
-    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
+def main():
+    query_text = parse()
+    results = retrieve_db(query_text=query_text)
+    print_scores(results)
 
-    model = ChatOpenAI()
-    response_text = model.predict(prompt)
+    # context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    # prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    # prompt = prompt_template.format(context=context_text, question=query_text)
+    # print(prompt)
+
+    # model = ChatOpenAI()
+    # response_text = model.predict(prompt)
 
 
     # sources = [doc.metadata.get("source", None) for doc, _score in results]
